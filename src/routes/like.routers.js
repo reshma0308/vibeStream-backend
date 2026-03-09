7166f95aa05639af2460311aaf398c19e71cc860
Reshma <reshmakri.2011@gmail.com>
2026-03-10 00:35:44 +0530

    fix router

--- src/routes/like.routers.js
+++ src/routes/like.routers.js
@@ -6,7 +6,7 @@ import {
 import { verifyJwt } from "../middlewares/auth.middleware.js";
 
 const router = Router();
-router.use(verifyJwt); // Apply verifyJWT middleware to all routes in this file
+router.use(verifyJwt); 
 
 router.route("/videos").get(getLikedVideos);
 router.route("/toggle/v/:videoId").post(toggleVideoLike);
--- src/routes/user.routers.js
+++ src/routes/user.routers.js
@@ -1,5 +1,5 @@
 import {Router} from "express"
-import { loginUser, logoutUser, registerUser, getUserProfileById } from "../controllers/user.controllers.js"
+import { loginUser, logoutUser, registerUser, getUserProfileById,refreshAccessToken } from "../controllers/user.controllers.js"
 import { upload } from "../middlewares/multer.middleware.js"
 import { verifyJwt } from "../middlewares/auth.middleware.js"
 
@@ -21,6 +21,6 @@ router.route("/register").post(
     registerUser)
 router.route("/login").post(loginUser)
 router.route("/logout").post(verifyJwt ,logoutUser)
-// router.route("/refresh-token").post()
+router.route("/refresh-token").post(refreshAccessToken)
 
 export default router
